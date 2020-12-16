54dca8dc0846a29bb11a1be5e3e2852b89386009
Sam Lansford <sam.j.lansford@gmail.com>
2020-12-16 11:43:11 -0600

    Not so great outcome

--- solutions.js
+++ solutions.js
@@ -20,3 +20,52 @@
  * and
  * concat
  */
+
+function isTrue(x) {
+  return x === true;
+}
+
+function isFalse(x) {
+    return x === false;
+}
+
+function isEven(x) {
+    x = Number(x);
+    return x === 0 || !!(x && !(x%2));
+}
+
+function addOne(x) {
+    x = Number(x);
+    return x + 1
+}
+
+function isEqual(x, y) {
+    x = parseInt("x");
+    return x  == true;
+}
+
+function not(x) {
+    if (x !== null) return true;
+    if (x !== Number) return false;
+}
+
+function isIdentical(x, y) {
+    if (x === y) return true;
+    if (x == null || y == null) return false;
+    if (x.length !== y.length) return false;
+    if (x == Number || y == Number) return true;
+}
+
+function and(x) {
+    if (x == true && true) return true;
+    if (x == true && false) return  false;
+}
+
+function concat(x) {
+
+}
+
+function or(x) {
+    if(x == true || false) return true;
+    if(x == false || false) return false;
+}
