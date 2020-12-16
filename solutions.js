fcf20f1a7131008a26a6d936f84be9dd8d817042
Yasin Mogultay <yasin.mglty@gmail.com>
2020-12-16 12:04:55 -0600

    Completed assesment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,64 @@
  * and
  * concat
  */
+// 1
+function isTrue (input){
+    return input === true;
+}
+isTrue();
+
+//2
+function  isFalse (input1) {
+    return input1 === false;
+}
+isFalse();
+
+//3
+function not(input3) {
+    return !input3 === true;
+}
+not();
+
+//4
+function addOne (input4) {
+    return ++input4;
+}
+addOne();
+
+//5
+
+function isEven (input5){
+    return (input5 % 2 == 0);
+}
+isEven();
+
+//6
+
+function isIdentical (input6, input7) {
+    return input6 === input7;
+}
+isIdentical();
+
+//7
+function isEqual (input8, input9) {
+    return input8 == input9;
+}
+isEqual();
+
+//8
+ function or(input10, input11) {
+     return input10 || input11;
+ }
+ or();
+
+ //9
+function and(input12, input13) {
+    return input12 && input13;
+}
+and();
+
+//10
+function concat (input14, input15) {
+    return input14 + "" + input15;
+}
+concat();
