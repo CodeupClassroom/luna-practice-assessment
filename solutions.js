2c5f7d4c1a1e01dbe41e07e9a4324aeb19238838
Carlos Padilla <mr.carlos.padilla@gmail.com>
2020-12-16 11:42:15 -0600

    adding practice assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,53 @@
  * and
  * concat
  */
+
+//1
+function isTrue(input) {
+    return input === true;
+}
+
+//2
+function isFalse(input) {
+    return input === false;
+}
+
+//3
+function not(input) {
+    return !input === true;
+}
+
+//4
+function addOne(input) {
+    return parseFloat(input) + 1;
+}
+
+//5
+function isEven(input) {
+    return input % 2 == 0;
+}
+
+//6
+function isIdentical(input1, input2) {
+    return input1 === input2;
+}
+
+//7
+function isEqual(input1, input2) {
+    return input1 == input2
+}
+
+//8
+function or(input1, input2) {
+    return input1 || input2;
+}
+
+//9
+function and(input1, input2) {
+    return input1 && input2;
+}
+
+//10
+function concat (input1, input2) {
+    return ("" + input1 + input2);
+}
