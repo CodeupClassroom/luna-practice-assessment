32aa208dab2827883956a39fd52faa9162d33377
Colin Warner <colin.x.warner@gmail.com>
2020-12-16 11:41:42 -0600

    practice assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,61 @@
  * and
  * concat
  */
+function isTrue(anyInput) {
+   if (anyInput === true) {
+       return true;
+   } else {
+       return false
+   }
+}
+
+function isFalse(inputProvided) {
+    if (inputProvided === false) {
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+function not(anyInput2) {
+        return !(anyInput2);
+}
+
+function addOne(singleInput) {
+        return Number(singleInput) + 1
+}
+
+function isEven(singleInput2) {
+    return singleInput2 % 2 == 0;
+// https://stackoverflow.com/questions/6211613/testing-whether-a-value-is-odd-or-even
+}
+
+function isIdentical(input1, input2) {
+    if(input1 === input2) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function isEqual(inputX, inputY) {
+    if (inputX == inputY) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function or(inputA, inputB) {
+    return (inputA || inputB);
+}
+
+function and(inputC, inputD) {
+    return (inputC && inputD);
+}
+
+function concat(inputE, inputF) {
+    return "" + inputE + inputF
+}
